d271a4b550a105c2ba1bc79435592a2483a2648a
ChenMike <mike@guidercard.com>
2026-01-12 16:19:16 +0800

    build測試2

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+    base: "/vue3-bff-web/",
   server: {
     proxy: {
       '/api': {
